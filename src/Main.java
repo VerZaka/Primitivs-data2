377d39e0fa007d8a4c629a8dba43c4792de492a3
Verona <valetta7777@list.ru>
2023-05-24 22:25:06 +0300

    Gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-       int Balance = 300;
-       int Amount = 1100;
-       int bonusRub = 1;
+        int Balance = 300;
+        int Amount = 1100;
+        int bonusRub = 1;
 
-    if (Amount > 1000) {
-        int bonusAmount = (Amount / 300);
+        if (Amount > 1000) {
+            int bonusAmount = (Amount / 300);
 
-        int bonus = bonusAmount * bonusRub;
+            int bonus = bonusAmount * bonusRub;
 
-        int totalBalance = Balance + Amount + bonus;
-        System.out.println("Итоговый счет " + totalBalance + " рублей");
-        System.out.println("Бонусный счет " + bonus + " рублей");
-    }
-    else {
-        int totalBalance = Balance + Amount;
-        System.out.println("Итоговый счет " + totalBalance + " рублей");
-        System.out.println("Бонусные рубли: 0 рублей.");
-    }
+            int totalBalance = Balance + Amount + bonus;
+            System.out.println("Итоговый счет " + totalBalance + " рублей");
+            System.out.println("Бонусный счет " + bonus + " рублей");
+        } else {
+            int totalBalance = Balance + Amount;
+            System.out.println("Итоговый счет " + totalBalance + " рублей");
+            System.out.println("Бонусные рубли: 0 рублей.");
+        }
 
     }
 }
